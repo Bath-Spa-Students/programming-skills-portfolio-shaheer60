f47320941d8caee0a95c38d41bb9969a3fdaafc8
shaheer60 <145544187+shaheer60@users.noreply.github.com>
2023-11-18 19:04:09 +0400

    chapter4 exercises

---
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -0,0 +1,12 @@
+alien_color = 'green'                                                            #if block
+if alien_color == 'green':
+    print('you earned 5 pointes for killing the alien')
+else:
+    print('you earned 10 points')
+
+
+    alien_color == 'red'
+    if alien_color == 'green':                                             #else block
+        print('you earned 5 points for killing the alien')
+    else:
+       print('you earned 10 points')     
---
+++ Chapter 4- Control Flow/Exercises/exercise5.py
@@ -0,0 +1,17 @@
+#Favourite fruits and then printing of the messages
+favorite_fruits = ["mangoes", "pineapples", "watermelons"]
+
+if "mangoes" in favorite_fruits:
+    print("You really like mangoes!")
+
+if "strawberries" in favorite_fruits:
+    print("You really like strawberries!")
+
+if "kiwi" in favorite_fruits:
+    print("You really like kiwi!")
+
+if "pineapples" in favorite_fruits:
+    print("You really like pineapples!")
+
+if "grapes" in favorite_fruits:
+    print("You really like grapes!")
